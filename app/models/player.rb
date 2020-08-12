f93a437318d2ab6d10fe5d5551beee33c699d97b
Jeferson Tonin <jefersontonin77@gmail.com>
2020-08-12 16:00:20 -0300

    added relations and validations to models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,12 @@
 class Player < ApplicationRecord
+  before_create :set_start_date
+
   belongs_to :movie
   belongs_to :user
+  
+  private
+
+  def set_start_date
+    self.start_date ||= Time.zone.now
+  end
 end
