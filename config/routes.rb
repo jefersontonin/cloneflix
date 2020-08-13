5a3a286671ad985f4be3f943116565e9fb103c61
Jeferson Tonin <jefersontonin77@gmail.com>
2020-08-13 16:18:50 -0300

    generate controllers and setup routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/dashboard', to: 'dashboards#index', as: 'dashboard'
+      resources :favorites, path: "my_list", only: %i( index create )
+      delete '/my_list/:type/:id', to: 'favorites#destroy'
+      resources :reviews, only: [:index, :create]
+      resources :searches, path: "search", only: :index
+      resources :series, only: :show
+      resources :movies, only: :show do
+        member do
+          get '/executions', to: 'executions#show'
+          put '/executions', to: 'executions#update'    
+        end
+      end
+      resources :recommendations, only: :index
+    end
+  end
+  root :to => "home#index"
 end
